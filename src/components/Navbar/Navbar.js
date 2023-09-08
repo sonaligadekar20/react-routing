0b5034805d63cb245f5816e4a9e7c4f81e07c538
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-09-09 00:25:49 +0530

    Navbar Created

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+    return(
+        <div className="nav">
+            <h1>Navbar</h1>
+        </div>
+    )
+  
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import Navbar from "./components/Navbar/Navbar"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+    <Navbar/>
+
+    </>
+)
