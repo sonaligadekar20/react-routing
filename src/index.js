de55f1e4e6794508ee0bd92c671ee98453c329a0
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-09-09 22:28:38 +0530

    changes added

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function App() {
         case "/contact":
             return <Contact />;
         default:
-            return <h1>404 Page Not Found</h1>;
+            return <h1> Page Not Found</h1>;
     }
 }
 
